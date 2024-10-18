c58328edd57540708291ca995caffbd9cbac9d1a
unknown <akeladumindu2000@gmail.com>
2024-10-18 11:11:41 +0530

    added initial styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+// node modules 
+
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
+// components 
+import App from './App.jsx';
+
+//css
+import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
