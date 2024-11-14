19dc6f0d2a4f9c6862e60bfdd2c14149676386a0
Akela Dumindu <128671284+AkelaDumindu@users.noreply.github.com>
2024-11-15 04:08:57 +0530

    changed card styles

--- src/components/portfolio/PortfolioCard.jsx
+++ src/components/portfolio/PortfolioCard.jsx
@@ -1,41 +1,45 @@
-import React from 'react'
+import React from 'react';
 import PropTypes from 'prop-types';
 
 const PortfolioCard = (props) => {
   return (
     <div className='card-outer'>
-        <div className='card-inner'>
+      <div className='card-inner'>
         <div>
-                <h1>
-                    {props.typeName}
-                </h1>
-
-            </div>
-            <div>
-                <img className='h-25' src={props.url} alt={props.label} />
-            </div>
-
-            <div>
-                <h2>{props.title}</h2>
-                <p>{props.description}</p>
-            </div>
-            <div>
-                {props.logo}
-            </div>
+          <h1>{props.typeName}</h1>
         </div>
-           
-    </div>   
-  )
-}
+        <div>
+          <img className='h-25' src={props.url} alt={props.label} />
+        </div>
+        <div>
+          <h2>{props.title}</h2>
+          <p>{props.description}</p>
+        </div>
+        <div className="icon-container">
+          {/* Render each icon passed as a prop */}
+          {props.icons && (
+            <div className="icons-wrapper">
+              {props.icons.map((icon, index) => (
+                <div key={index} className="icon-wrapper">
+                  {icon}
+                  <span className="icon-text">{icon.props.alt || 'Icon'}</span>
+                </div>
+              ))}
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
 
-PortfolioCard.PropTypes = {
-    typeName:PropTypes.string.isRequired,
-    url:PropTypes.string.isRequired,
-    label:PropTypes.string.isRequired,
-    title:PropTypes.string.isRequired,
-    description:PropTypes.string.isRequired,
-    logo:PropTypes.isRequired,
+PortfolioCard.propTypes = {
+  typeName: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  icons: PropTypes.arrayOf(PropTypes.element),  // Array of React elements (icons)
+};
 
-}
-
-export default PortfolioCard
+export default PortfolioCard;
