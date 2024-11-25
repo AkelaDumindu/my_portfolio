8842cc2868c5975339f4e4d423109ddb21cf04d6
Akela Dumindu <128671284+AkelaDumindu@users.noreply.github.com>
2024-11-26 01:29:04 +0530

    about me section first

--- src/components/about/MyAbout.jsx
+++ src/components/about/MyAbout.jsx
@@ -1,37 +1,20 @@
+import './about.css';
+import Resume from './Resume';
 
-
-const aboutItems = [
-    {
-      label: 'Project done',
-      number: 12
-    },
-    {
-      label: 'No of Skills',
-      number: 10
-    }
-  ];
 const MyAbout = () => {
   return (
     <section id='about' className='section'>
         <div className="container ">
             <div className='bg-zinc-800/50 p-7 rounded-2xl md:p-12 about-info'>
-                <p className='text-zinc-300 mb-4 md:mb-8 md:text-xl md:max-w-[60ch]'>Welcome! I&apos;m Akela, a professional web developer with a knack for crafting visually stunning and highly functional websites. Combining creativity and technical expertise. I transform your vision into digital masterpiece that excels in both appearance and performance.</p>
 
-                <div className='flex flex-wrap items-center gap-4 md:gap-7'>
-                    {
-                        aboutItems.map(({label, number}, key)=>(
-                            <div key={key}>
-                                <div className='flex items-center md:mb-2'>
-                                    <span className='text-2xl font-semibold md:text-4xl'>{number}</span>
-                                    <span className='text-sky-400 font-semibold md:text-3xl '>+</span>
-                                </div>
-                                <p className='text-sm text-zinc-400'>{label}</p>
-                            </div>
-                        ))
-                    }
+            <div className="flex flex-col items-center mb-5">
+            <h2 className="headline-2">About Me</h2>
+          </div>
+                <p className='about-des text-zinc-300 mb-4 md:mb-8 md:text-xl md:max-w-[60ch]'>
+                I am a Software Engineering undergraduate with experience in full-stack web and mobile application development, solving complex problems. By leveraging various programming languages, frameworks, and libraries, I create innovative solutions for real-world challenges. Seeking a Software Engineering internship to enhance my skills and contribute to impactful projects in a dynamic environment.
+                </p>
 
-                    <img src="\images\fav\apple-touch-icon.png" width={30} height={30} className='ml-auto md:w-[40px] md:h-[40px]' alt="akela dumindu logo" />
-                </div>
+                <Resume />
             </div>
         </div>
     </section>
