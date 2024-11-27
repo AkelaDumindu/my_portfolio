ec0faf66a384dd1abe751f6b0b636b40fa210c09
Akela Dumindu <128671284+AkelaDumindu@users.noreply.github.com>
2024-11-27 20:45:30 +0530

    created experience section

--- src/components/about/Experience.jsx
+++ src/components/about/Experience.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
+import {motion} from "framer-motion"
+import ResumeCard from "./ResumeCard";
+import './resumeCard.css';
 
 const Experience = () => {
   return (
-    <div>Experience</div>
-  )
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1, transition: { duration: 0.5 } }}
+      className="py-12 font-titleFont flex gap-20 w-full"
+    >
+      <div className="w-full">
+        <div className="flex flex-col gap-4">
+         
+          <h2 className="text-4xl font-bold">My Experience</h2>
+        </div>
+        <div className="timeline-container">
+          <ResumeCard
+            title="Internship Trainee"
+            subTitle="Bank Of Ceylon"
+            year="2020 March - 2020 September"
+            logo="../images/resumeImages/boc_logo.png"
+          />
+          
+        </div>
+      </div>
+      
+        
+    </motion.div>
+  );
 }
 
 export default Experience
